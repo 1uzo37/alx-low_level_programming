fe34019bdd65ff1d95f9786528483ed30e847f9b
emmanuel_uzowuru <emmanueluzowuru37@gmail.com>
2022-09-11 05:18:56 -0700

    Print all EX

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,15 +6,16 @@
  * this program prints 
  * Return: is 0
  */
+
 int main(void)
 {
-	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
 
-	if (ch != 'e' && ch != 'q')
-        {
-		putchar(ch);
-	}
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+}
 putchar('\n');
 return (0);
 }
